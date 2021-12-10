123a4ef36a4f4302f659bcd07a55222082468c1a
Brandon Lodowica <brandonlodowica@hotmail.com>
2021-12-10 03:43:09 +0100

    startup reverted

--- Livestream Backend application/Livestream Backend application/Program.cs
+++ Livestream Backend application/Livestream Backend application/Program.cs
@@ -12,34 +12,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Livestream_Backend_application.Seed;
+using Livestream_Backend_application;
 
 namespace Livestream_Backend_application
 {
+
+
     public class Program
     {
-        public static async Task Main(string[] args)
+        public static void Main(string[] args)
         {
-            var host = CreateHostBuilder(args).Build();
-
-            //CreateHostBuilder(args).Build().Run();
-            using var scope = host.Services.CreateScope();
-
-            var services = scope.ServiceProvider;
-
-            try
-            {
-                var context = services.GetRequiredService<LivestreamDBContext>();
-                var userManager = services.GetRequiredService<UserManager<AppUser>>();
-                await context.Database.MigrateAsync();
-                await Seed.Seed.SeedData(context, userManager);
-            }
-            catch (Exception )
-            {
-
-                throw new Exception();
-            }
-
-            await host.RunAsync();
+            CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
