8c3fdce1bb1874a7949f21301fb2a994fb0999b8
Brandon Lodowica <brandonlodowica@hotmail.com>
2022-01-19 13:25:11 +0100

    removed azure signal r package

--- Livestream Backend application/LiveStreamBackend.Test/BackendTest/TestContext.cs
+++ Livestream Backend application/LiveStreamBackend.Test/BackendTest/TestContext.cs
@@ -3,10 +3,11 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using Livestream_Backend_application.Models;
+using Microsoft.EntityFrameworkCore.Design;
 
 namespace LiveStreamBackend.Test
 {
-    public class TestContext : IDbContextFactory<LivestreamDBContext>
+    public class TestContext : IDesignTimeDbContextFactory<LivestreamDBContext>
     {
         public DbContextOptions<LivestreamDBContext> options { get; private set; }
 
@@ -21,6 +22,30 @@ namespace LiveStreamBackend.Test
         {
             return new LivestreamDBContext(options);
         }
+
+        public LivestreamDBContext CreateDbContext(string[] args)
+        {
+            throw new NotImplementedException();
+        }
     }
+
+
+    public class TestContextFactory : IDesignTimeDbContextFactory<LivestreamDBContext>
+    {
+        public DbContextOptions<LivestreamDBContext> options { get; private set; }
+
+        public TestContextFactory()
+        {
+            options = new DbContextOptionsBuilder<LivestreamDBContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+        }
+
+        public LivestreamDBContext CreateDbContext()
+        {
+            return new LivestreamDBContext(options);    
+        }
+    }
+
 }
 */
